8145e4cb14ed174cf0fe37edd595df3e2b9debb2
Leegunhee777 <akanfldkdlel@ajou.ac.kr>
2022-07-31 03:12:11 +0900

    feat: 컬렉션 캡슐화하기

--- ch7/7-2.solution.js
+++ ch7/7-2.solution.js
@@ -11,14 +11,22 @@ export class Person {
   }
 
   get courses() {
-    return this.#courses;
+    return [...this.#courses];
   }
 
-  set courses(courses) {
-    this.#courses = courses;
+  addCourse(course) {
+    this.#courses.push(course);
+  }
+
+  removeCourse(course, runIfAbsent) {
+    const index = this.#courses.indexOf(course);
+    if (index === -1) {
+      runIfAbsent();
+      return;
+    }
+    this.#courses.splice(index, 1);
   }
 }
-
 export class Course {
   #name;
   #isAdvanced;
@@ -37,11 +45,10 @@ export class Course {
 }
 
 const ellie = new Person('엘리');
-ellie.courses.push(new Course('리팩토링', true));
+const course = new Course('리팩토링', true);
+ellie.addCourse(course);
 console.log(ellie.courses.length);
-
-/*
-<컬렉션 캡슐화 하기, 컬렉션: 리스트, 배열형태의 것들>
-ellie.course 를통해 push하고 delete하고 마음대로 내부value를 어떤 조작이던 마음대로 컨트롤하게된다
-이는 굉장히 위험하다 (외부에서 내부 데이터를 마음대로 조작??그것은 위험하다)
-*/
+ellie.removeCourse(course, () => {
+  console.log('해당 코스는 없다.');
+});
+console.log(ellie.courses.length);
