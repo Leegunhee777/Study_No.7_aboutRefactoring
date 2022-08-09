eecee095fd08f5b016531e5a54041fa8df9eb88f
Leegunhee777 <akanfldkdlel@ajou.ac.kr>
2022-08-10 01:16:19 +0900

    feat: 리터럴로 바꾸기

--- ch9/9-6.js
+++ ch9/9-6.js
@@ -1,3 +1,13 @@
 function potentialEnergy(mass, height) {
   return mass * 9.81 * height;
 }
+
+const STANDARD_GRAVITY = 9.81;
+function afterPotentialEnergy(mass, height) {
+  return mass * STANDARD_GRAVITY * height;
+}
+
+/*
+<매직 리터럴 바꾸기>
+코드에서 위미없는 숫자 9.81 같은 뭔지 모르겠는 숫자를보면 아주 불쾌하다
+*/
