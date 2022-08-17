34a88ed86a28b40bfe60ae87efc96e9ffe66f230
Leegunhee777 <akanfldkdlel@ajou.ac.kr>
2022-08-18 01:45:46 +0900

    feat: 플래스 인수 제거하기

--- ch11/11-3.js
+++ ch11/11-3.js
@@ -17,3 +17,8 @@ class Concert {
 
 // 예제 3
 function setSwitch(on);
+
+/*
+<플래그 인수 제거하기> : boolean값을 인수로 넘기는것, 인수를 boolean으로 넘기면
+함수의 의도에 대해 파악하기 어렵기 때문
+*/
---
+++ ch11/11-3.solution.js
@@ -0,0 +1,20 @@
+// 예제 1
+
+function setWidth(value) {
+  this._width = value;
+}
+
+function setHeight(value) {
+  this._height = value;
+}
+
+// 예제 2
+class Concert {
+    regularBook(customer){}
+    premiumBook(customer){}
+  }
+  
+  // 예제 3
+function switchOn();
+function switchOff();
+  
