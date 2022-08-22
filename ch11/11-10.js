a804ff6817a5a915357c91c21d4796356e65f634
Leegunhee777 <akanfldkdlel@ajou.ac.kr>
2022-08-23 01:33:54 +0900

    feat: 명령객체를 함수로 바꾸기

--- ch11/11-10.js
+++ ch11/11-10.js
@@ -11,3 +11,10 @@ export class ChargeCalculator {
     return this.baseCharge + this._provider.connectionCharge;
   }
 }
+
+/*
+<명령 객체를 함수로 바꾸기>
+재사용성을 전혀 고려할 필요가 없는 일시적으로 사용하는 경우 굳이 명령객체로 만들필요없음
+왜냐하면 클래스로 만들면 사용할때 매번 인스턴스를만들어, 내부 함수를 호출해야하는데,
+재사용되지 않는데 인스턴스를 매번 생성하는것은 불필요한 메모리 낭비가될수있다.
+*/
