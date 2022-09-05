a065161e74b9f90f47fc6d1987726ce334114864
Leegunhee777 <akanfldkdlel@ajou.ac.kr>
2022-09-06 02:25:31 +0900

    feat: 다형성 이용하기

--- ch12/test.js
+++ ch12/test.js
@@ -33,6 +33,8 @@ class Sub extends Printer {
   }
 
   qwe() {
+    console.log(super.id);
+    console.log(this.id);
     console.log(super.name);
     console.log('ewq');
   }
@@ -40,3 +42,27 @@ class Sub extends Printer {
 
 const printer = new Printer(1, 'asd');
 const sub = new Sub('서울');
+
+class Parent {
+  _name;
+  constructor(name) {
+    this._name = name;
+  }
+  get name() {
+    return this._name;
+  }
+}
+
+class Child extends Parent {
+  qwe() {
+    console.log(this._name, '111');
+    console.log(super.name, '222');
+  }
+}
+
+//자식에서 contrutor사용하면 무조건 자식생정자 내부에서 super()로 부모생성자 호출해줘야함
+//자식에서 contrutor사용안하면 그냥 defualt로 부모 생성자만 호출됨
+
+//어찌됬던 부모생성자는 호출되는건데 자식클래스의 생성자에서 명시적명시냐 자동으로하냐의 차이
+const qwe = new Child('qqq');
+console.log(qwe.qwe());
--- challenge/statement.solution.js
+++ challenge/statement.solution.js
@@ -2,58 +2,75 @@ export function statement(invoice, plays) {
   const statement = createStatement(invoice, plays);
   return renderPlainText(statement);
 }
+class Performance {
+  #audience;
+  #play;
+  constructor(audience, play) {
+    this.#audience = audience;
+    this.#play = play;
+  }
+
+  get play() {
+    return this.#play;
+  }
+
+  get audience() {
+    return this.#audience;
+  }
+
+  static create(audience, play) {
+    switch (play.type) {
+      case 'tragedy':
+        return new Tragedy(audience, play);
+
+      case 'comedy':
+        return new Comedy(audience, play);
+
+      default:
+        throw new Error('알수없는 타입니다.');
+    }
+  }
+}
+
+class Tragedy extends Performance {
+  get amount() {
+    let result = 40000;
+    if (this.audience > 30) {
+      result += 1000 * (this.audience - 30);
+    }
+    return result;
+  }
+  get credits() {
+    return Math.max(this.audience - 30, 0);
+  }
+}
+
+class Comedy extends Performance {
+  get amount() {
+    let result = 30000;
+    if (this.audience > 20) {
+      result += 10000 + 500 * (this.audience - 20);
+    }
+    result += 300 * this.audience;
+    return result;
+  }
+  get credits() {
+    let result = Math.max(this.audience - 30, 0);
+    result += Math.floor(this.audience / 5);
+    return result;
+  }
+}
 
 function createStatement(invoice, plays) {
   const statement = {};
   statement.customer = invoice.customer;
-  statement.performances = invoice.performances.map(p => enrichPerformance(p));
+  statement.performances = invoice.performances.map(p =>
+    Performance.create(p.audience, plays[p.playID])
+  );
+
   statement.totalAmount = totalAmount(statement.performances);
   statement.totalCredits = totalCredits(statement.performances);
   return statement;
-
-  function enrichPerformance(performance) {
-    const result = { ...performance };
-    result.play = playFor(performance);
-    result.amount = amountFor(result);
-    result.credits = creditsFor(result);
-    return result;
-  }
-  function playFor(performance) {
-    return plays[performance.playID];
-  }
-
-  function amountFor(performance) {
-    let result = 0;
-    switch (performance.play.type) {
-      case 'tragedy': // 비극
-        result = 40000;
-        if (performance.audience > 30) {
-          result += 1000 * (performance.audience - 30);
-        }
-        break;
-      case 'comedy': // 희극
-        result = 30000;
-        if (performance.audience > 20) {
-          result += 10000 + 500 * (performance.audience - 20);
-        }
-        result += 300 * performance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${performance.play.type}`);
-    }
-    return result;
-  }
-
-  function creditsFor(performance) {
-    let result = 0;
-    result += Math.max(performance.audience - 30, 0);
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === performance.play.type) {
-      result += Math.floor(performance.audience / 5);
-    }
-
-    return result;
-  }
 
   function totalAmount(performances) {
     return performances.reduce((sum, p) => (sum += p.amount), 0);
